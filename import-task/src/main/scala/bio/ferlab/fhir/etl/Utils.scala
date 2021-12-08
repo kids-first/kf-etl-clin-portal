7e8e041e03de2581ec1729950685b8ea2087e4b3
Celine Pelletier <cpelletier@ferlab.bio>
2021-12-08 16:43:37 -0500

    WIP Genomic files

--- import-task/src/main/scala/bio/ferlab/fhir/etl/Utils.scala
+++ import-task/src/main/scala/bio/ferlab/fhir/etl/Utils.scala
@@ -8,8 +8,9 @@ import scala.reflect.ClassTag
 object Utils {
 
   val actCodeR = "^phs[0-9.a-z]+"
+  val studyCodePattern = "^SD_[0-9A-Za-z]+"
 
-  private def codingSystemClassify(url : String) = {
+  private def codingSystemClassify(url: String) = {
     url match {
       case "http://purl.obolibrary.org/obo/mondo.owl" => "MONDO"
       case "https://www.who.int/classifications/classification-of-diseases" => "ICD"
@@ -20,7 +21,7 @@ object Utils {
   }
 
   val extractAclFromList: UserDefinedFunction =
-    udf((arr: Seq[String]) => arr.filter(e => e matches actCodeR))
+    udf((arr: Seq[String]) => arr.filter(e => (e matches actCodeR) || (e matches studyCodePattern)))
 
   val codingClassify: UserDefinedFunction =
     udf((arr: Seq[(String, String, String, String, String, String)]) => arr.map(r => (codingSystemClassify(r._2), r._4)))
@@ -28,15 +29,21 @@ object Utils {
   def firstNonNull: UserDefinedFunction = udf((arr: Seq[String]) => arr.find(_ != null).orNull)
 
   val ncitIdAnatomicalSite: UserDefinedFunction =
-    udf((arr: Seq[(String, String, String, String, String, Boolean)]) => arr.find(r => r._2 matches "ncit\\.owl$" ) match {
+    udf((arr: Seq[(String, String, String, String, String, Boolean)]) => arr.find(r => r._2 matches "ncit\\.owl$") match {
       case Some(v) => v._4
       case None => null
     })
 
   val uberonIdAnatomicalSite: UserDefinedFunction =
-    udf((arr: Seq[(String, String, String, String, String, Boolean)]) => arr.find(r => r._2 matches "uberon\\.owl$" ) match {
+    udf((arr: Seq[(String, String, String, String, String, Boolean)]) => arr.find(r => r._2 matches "uberon\\.owl$") match {
       case Some(v) => v._4
       case None => null
     })
 
+  val extractHashes: UserDefinedFunction =
+    udf(
+      (arr: Seq[(Option[String], Seq[(Option[String], Option[String], Option[String], Option[String], Option[String], Option[Boolean])], Option[String])])
+        => arr.map(r => r._2(0)._5 -> r._3).toMap)
+
+  val retrieveIsHarmonized: UserDefinedFunction = udf((s: Option[String]) => s.exists(_.contains("harmonized-data")))
 }
