8afb9dd959cfe90b749a78321d3cf883995f167d
adrian <adipaul1981@yahoo.ca>
2021-12-15 17:15:45 -0500

    join phenotypes on participant

---
+++ prepare-index/src/main/scala/bio/ferlab/fhir/etl/common/OntologyUtils.scala
@@ -0,0 +1,16 @@
+package bio.ferlab.fhir.etl.common
+
+import org.apache.spark.sql.expressions.UserDefinedFunction
+import org.apache.spark.sql.functions._
+
+object OntologyUtils {
+
+//TODO add age_at_event_days field
+  val transformAncestors: UserDefinedFunction =
+    udf((arr: Seq[(String, String, Seq[String])]) => arr.map(a => (s"${a._2} (${a._1})", a._3, false, false)))
+
+  //TODO add age_at_event_days field
+  val transformTaggedPhenotype: UserDefinedFunction =
+    udf((id: String, name: String, parents: Seq[String], is_leaf: Boolean) =>  (s"${name} (${id})", parents, true, is_leaf))
+
+}
--- prepare-index/src/main/scala/bio/ferlab/fhir/etl/common/Utils.scala
+++ prepare-index/src/main/scala/bio/ferlab/fhir/etl/common/Utils.scala
@@ -1,7 +1,8 @@
 package bio.ferlab.fhir.etl.common
 
+import bio.ferlab.fhir.etl.common.OntologyUtils.{transformAncestors, transformTaggedPhenotype}
 import org.apache.spark.sql.expressions.UserDefinedFunction
-import org.apache.spark.sql.functions.{array, col, collect_list, filter, map, size,struct, udf}
+import org.apache.spark.sql.functions.{array, array_union, col, collect_list, filter, lit, map, size, struct, transform, udf}
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
 object Utils {
@@ -12,6 +13,9 @@ object Utils {
       case "positive" => (s"$source_text ($code)", null, source_text, null, true)
       case _ => (null, s"$source_text ($code)", null, source_text, false)
     })
+
+  val observableTileStandard: UserDefinedFunction =
+    udf((term: String) => term.replace("_", ":"))
 
   implicit class DataFrameOperations(df: DataFrame) {
     def addStudy(studyDf: DataFrame): DataFrame = {
@@ -26,11 +30,16 @@ object Utils {
       val reformatBiospecimen: DataFrame = biospecimensDf
         .withColumn("biospecimen", struct(biospecimensDf.columns.map(col): _*))
         .select("participant_fhir_id", "biospecimen")
+        .groupBy("participant_fhir_id")
+        .agg(
+          collect_list(col("biospecimen")) as "biospecimens")
 
-      df.join(reformatBiospecimen, col("fhir_id") === col("participant_fhir_id"))
+      df
+        .join(reformatBiospecimen, col("fhir_id") === col("participant_fhir_id"))
+        .drop("fhir_id")
     }
 
-    def addDiagnosysPhenotypes(conditionDf: DataFrame): DataFrame = {
+    def addDiagnosysPhenotypes(conditionDf: DataFrame)(hpoTerms: DataFrame): DataFrame = {
       val phenotypes = conditionDf
         .select("*").where("""condition_profile == "phenotype"""")
         //filter out phenopype with empty code
@@ -41,27 +50,40 @@ object Utils {
         .withColumn("hpo_phenotype_observed_text", col("phenotype_code_text")("_3"))
         .withColumn("hpo_phenotype_not_observed_text", col("phenotype_code_text")("_4"))
         .withColumn("observed_bool", col("phenotype_code_text")("_5"))
-        .groupBy("participant_fhir_id")
-        .agg(
-          collect_list(struct(
-            col("fhir_id"),
-            col("hpo_phenotype_observed"),
-            col("hpo_phenotype_not_observed"),
-            col("hpo_phenotype_observed_text"),
-            col("hpo_phenotype_not_observed_text"),
-            col("observed_bool") as "observed"
-          )) as "phenotype"
-        )
+        .withColumn("observable_term", observableTileStandard(col("condition_coding")(0)("_2")))
+        .join(hpoTerms, col("observable_term") === col("id"))
+        .withColumn("transform_ancestors", transformAncestors(col("ancestors")))
+        .withColumn("transform_tagged_phenotype", transformTaggedPhenotype(col("id"), col("name"),col("parents"), col("is_leaf") ))
+        .withColumn("phenotype_with_ancestors", array_union(col("transform_ancestors"), array(col("transform_tagged_phenotype"))))
+        .drop("transform_ancestors", "transform_tagged_phenotype", "ancestors", "id", "is_leaf", "name", "parents")
+      //        .groupBy("participant_fhir_id")
+//        .agg(
+//          collect_list(struct(
+//            col("fhir_id"),
+//            col("hpo_phenotype_observed"),
+//            col("hpo_phenotype_not_observed"),
+//            col("hpo_phenotype_observed_text"),
+//            col("hpo_phenotype_not_observed_text"),
+//            col("observed_bool") as "observed"
+//          )) as "phenotype"
+//        )
 
-      val diagnosis: DataFrame = conditionDf
-        .select("*").where("""condition_profile == "disease"""")
+//      val toto = df.join(phenotypes, "participant_fhir_id")
+
       phenotypes.show(false)
+
+      phenotypes
+
+//      val diagnosis: DataFrame = conditionDf
+//        .select("*").where("""condition_profile == "disease"""")
+//      phenotypes.show(false)
+//      df.show(false)
 //      phenotypes.printSchema()
-      diagnosis.show(false)
+//      diagnosis.show(false)
 
 
-      df
     }
+
   }
 
 
