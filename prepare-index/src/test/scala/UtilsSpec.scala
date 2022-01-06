db6b946ceb17c8e7fd7f0b3dbc4df48e62304531
adrian <adipaul1981@yahoo.ca>
2022-01-06 14:25:31 -0500

    Add phenotype test observed and not

--- prepare-index/src/test/scala/UtilsSpec.scala
+++ prepare-index/src/test/scala/UtilsSpec.scala
@@ -1,5 +1,7 @@
+import bio.ferlab.datalake.spark3.loader.GenericLoader.{insert, read}
 import bio.ferlab.fhir.etl.common.Utils._
-import model.{BIOSPECIMEN, PARTICIPANT, PARTICIPANT_CENTRIC, STUDY}
+import model.{BIOSPECIMEN, CONDITION, CONDITION_CODING, PARTICIPANT, PARTICIPANT_CENTRIC, PHENOTYPE, STUDY}
+import org.apache.spark.sql.Row
 import org.scalatest.{FlatSpec, Matchers}
 
 
@@ -42,5 +44,31 @@ class UtilsSpec extends FlatSpec with Matchers with WithSparkSession {
 
     participantA._2 shouldEqual Seq("1", "2")
     participantB._2 shouldEqual Seq.empty[String]
+  }
+
+  "addDiagnosisPhenotypes" should "group phenotypes by observed or non-observed" in {
+    val allHpoTerms = read("./prepare-index/src/test/resources/hpo_terms.json", "Json", Map(), None, None)
+    val allMondoTerms = read("./prepare-index/src/test/resources/mondo_terms.json", "Json", Map(), None, None)
+
+    val inputParticipants = Seq(
+      PARTICIPANT(participant_id = "A", fhir_id = "A"),
+      PARTICIPANT(participant_id = "B", fhir_id = "B")
+    ).toDF()
+
+    val inputConditions = Seq(
+      CONDITION(fhir_id = "1", participant_fhir_id = "A", condition_coding = Seq(CONDITION_CODING(`category` = "HPO", `code` = "HP_0001631")) ,observed = "positive", condition_profile = "phenotype"),
+      CONDITION(fhir_id = "2", participant_fhir_id = "A", condition_coding = Seq(CONDITION_CODING()), condition_profile = "phenotype"),
+      CONDITION(fhir_id = "3", participant_fhir_id = "A", condition_coding = Seq(CONDITION_CODING()),observed = "not", condition_profile = "phenotype"),
+      CONDITION(fhir_id = "4", participant_fhir_id = "A", condition_profile = "phenotype")
+    ).toDF()
+
+    val output = inputParticipants.addDiagnosisPhenotypes(inputConditions)(allHpoTerms, allMondoTerms)
+
+    val participantPhenotypes = output.select("participant_id","phenotype").as[(String, Seq[PHENOTYPE])].collect()
+    val participantA = participantPhenotypes.filter(_._1 == "A").head
+    val participantB = participantPhenotypes.filter(_._1 == "B").head
+
+    participantA._2.map(p => (p.fhir_id, p.observed)) shouldEqual Seq(("1", true), ("2", false), ("3", false))
+    participantB._2.map(p => (p.fhir_id, p.observed)) shouldEqual Nil
   }
 }
---
+++ prepare-index/src/test/scala/model/PHENOTYPE.scala
@@ -0,0 +1,10 @@
+package model
+
+case class PHENOTYPE(
+                        `fhir_id`: String = "678509",
+                        `hpo_phenotype_observed`: String = "Patent ductus arteriosus (HP_0001643)",
+                        `hpo_phenotype_not_observed`: String = null,
+                        `hpo_phenotype_observed_text`: String = "Patent ductus arteriosus",
+                        `hpo_phenotype_not_observed_text`: String = null,
+                        `observed`: Boolean = false
+                      )
