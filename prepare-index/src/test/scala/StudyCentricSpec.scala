76885724c65ecc3becca38d518c899d1afcb0af0
Celine Pelletier <cpelletier@ferlab.bio>
2022-01-20 09:48:36 -0500

    Add test in case of no family data in study centric

--- prepare-index/src/test/scala/StudyCentricSpec.scala
+++ prepare-index/src/test/scala/StudyCentricSpec.scala
@@ -26,5 +26,21 @@ class StudyCentricSpec extends FlatSpec with Matchers with WithSparkSession {
     study_centric.as[STUDY_CENTRIC].collect() should contain theSameElementsAs
       Seq(STUDY_CENTRIC(`participant_count` = 2, `file_count` = 3, `family_count` = 2, `family_data` = true))
   }
+  "transform" should "prepare index study_centric with family_data false if no group" in {
+    val data: Map[String, DataFrame] = Map(
+      "normalized_researchstudy" -> Seq(RESEARCHSTUDY()).toDF(),
+      "normalized_patient" -> Seq(PARTICIPANT(), PARTICIPANT()).toDF(),
+      "normalized_documentreference" -> Seq(DOCUMENTREFERENCE(), DOCUMENTREFERENCE(), DOCUMENTREFERENCE()).toDF(),
+      "normalized_group" -> Seq[FAMILY]().toDF()
+    )
+
+    val output = new StudyCentric("re_00001")(conf).transform(data)
+
+    output.keys should contain("es_index_study_centric")
+
+    val study_centric = output("es_index_study_centric")
+    study_centric.as[STUDY_CENTRIC].collect() should contain theSameElementsAs
+      Seq(STUDY_CENTRIC(`participant_count` = 2, `file_count` = 3, `family_count` = 0, `family_data` = false))
+  }
 
 }
