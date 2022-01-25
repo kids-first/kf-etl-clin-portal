b643c295d70f70138994ed77f4342e5d871b64a3
Celine Pelletier <cpelletier@ferlab.bio>
2022-01-25 16:21:03 -0500

    Add arguments for release id and study ids for import task

--- import-task/README.md
+++ import-task/README.md
@@ -1,10 +1,32 @@
-## Known Issues
-*Refer to the Fhavro-export to setup your local environment
+# Import Task
 
-To develop locally with Minio, you need to setup the enviromnent variable as such:
+## Generate configuration file
 
-AWS_REGION=us-east-1;
-AWS_ACCESS_KEY=minioadmin;
-AWS_SECRET_KEY=minioadmin
+The config generator is located at: src/main/scala/bio/ferlab/fhir/etl/config/ConfigurationGenerator. Right-click and run.
 
-You need to provide as an argument to the program where the configuration file is (e.g: ./config/dev.conf).
+## Environment Variables (Mandatory)
+
+- `AWS_ACCESS_KEY` : The access key
+- `AWS_SECRET_KEY` : The secret key
+- `AWS_REGION`     : The region
+
+Example of a setup with Minio:
+- AWS_REGION=us-east-1;
+- AWS_ACCESS_KEY=minioadmin;
+- AWS_SECRET_KEY=minioadmin;
+
+## Arguments (Mandatory)
+
+1st argument: Path to configuration file `config/qa.conf` or `config/prod.conf` or `config/dev.conf`
+
+2nd argument: Steps to run for ETL `default`
+
+3rd argument: Release id
+
+4th argument: Study ids separated by `;`
+
+Example : `./config/dev.conf default RE_000001 SD_Z6MWD3H0;SD_Y6PXD3F0`
+
+## Launch the application
+
+The main application is located at: src/main/scala/bio/ferlab/fhir/etl/ImportTask. Right-click and run.
--- import-task/src/main/scala/bio/ferlab/fhir/etl/fhavro/FhavroToNormalizedMappings.scala
+++ import-task/src/main/scala/bio/ferlab/fhir/etl/fhavro/FhavroToNormalizedMappings.scala
@@ -3,7 +3,7 @@ package bio.ferlab.fhir.etl.fhavro
 import bio.ferlab.datalake.commons.config.{Configuration, DatasetConf, Format}
 import bio.ferlab.datalake.spark3.transformation.{Custom, Transformation}
 import bio.ferlab.fhir.etl.transformations.Transformations.extractionMappings
-import org.apache.spark.sql.functions.{col, regexp_extract}
+import org.apache.spark.sql.functions.{col, lit, regexp_extract}
 import play.api.libs.json.{JsValue, Json}
 
 import scala.util.matching.Regex
@@ -13,20 +13,20 @@ object FhavroToNormalizedMappings {
 
   val idFromUrlRegex = "https://kf-api-fhir-service.kidsfirstdrc.org/[A-Za-z]+/([0-9A-Za-z]+)/_history"
 
-
-
   val INGESTION_TIMESTAMP = "ingested_on"
 
-  val defaultTransformations: List[Transformation] = List(
-    Custom(_
+  def defaultTransformations(releaseId: String): List[Transformation] = {
+    List(Custom(_
       .withColumn("fhir_id", regexp_extract(col("id"), idFromUrlRegex, 1))
-    )
-  )
+      .withColumn("release_id", lit(releaseId))
+    ))
+  }
 
-  def mappings(implicit c: Configuration): List[(DatasetConf, DatasetConf, List[Transformation])] = c.sources.filter(s => s.format == Format.AVRO).map(s =>
-    {
+  def mappings(releaseId: String)(implicit c: Configuration): List[(DatasetConf, DatasetConf, List[Transformation])] = {
+    c.sources.filter(s => s.format == Format.AVRO).map(s => {
       val pattern(table) = s.id
-      (s, c.getDataset(s"normalized_$table"), defaultTransformations ++ extractionMappings(table))
+      (s, c.getDataset(s"normalized_$table"), defaultTransformations(releaseId) ++ extractionMappings(table))
     }
-  )
+    )
+  }
 }
