ac6061ffd208066624df68eb09f9425d56e0ff94
jecos <jcostanza@ferlab.bio>
2022-03-02 16:47:20 -0500

    Ad more scripts

--- bin/run_fhavro_export.sh
+++ bin/run_fhavro_export.sh
@@ -8,30 +8,23 @@ instance_count="1"
 if [ "$env" = "prd" ]
 then
   subnet="subnet-0cdbe9ba6231146b5"
+  es=""
 else
   subnet="subnet-0f1161ac2ee2fba5b"
+  es="https://vpc-include-arranger-blue-es-qa-xf3ttht4hjmxjfoh5u5x4jnw34.us-east-1.es.amazonaws.com"
 fi
 
 steps=$(cat <<EOF
 [
   {
-    "Args": [
-      "spark-submit",
-      "--deploy-mode",
-      "client",
-      "--class",
-      "bio.ferlab.fhir.etl.ImportTask",
-      "s3a://include-373997854230-datalake-${env}/jobs/import-task.jar",
-      "config/${env}.conf",
-      "default",
-      "${release_id}",
-      "${study_id}"
-    ],
-    "Type": "CUSTOM_JAR",
-    "ActionOnFailure": "TERMINATE_CLUSTER",
-    "Jar": "command-runner.jar",
-    "Properties": "",
-    "Name": "Import Task"
+    "Type":"CUSTOM_JAR",
+    "Name":"Cleanup jars",
+    "ActionOnFailure":"TERMINATE_CLUSTER",
+    "Jar":"command-runner.jar",
+    "Args":[
+      "bash","-c",
+      "sudo rm -f /usr/lib/spark/jars/spark-avro.jar"
+    ]
   }
 
 ]
@@ -45,12 +38,12 @@ aws emr create-cluster \
   --service-role include-datalake-emr-$env-role \
   --enable-debugging \
   --release-label emr-6.5.0 \
-  --bootstrap-actions Path="s3://include-373997854230-datalake-qa/jobs/bootstrap-actions/enable-ssm.sh" Path="s3://include-373997854230-datalake-qa/jobs/bootstrap-actions/cleanup-jars.sh" \
+  --bootstrap-actions Path="s3://include-373997854230-datalake-qa/jobs/bootstrap-actions/enable-ssm.sh" Path="s3://include-373997854230-datalake-${env}/jobs/bootstrap-actions/install-java11.sh" \
   --steps "${steps}" \
   --log-uri "s3n://include-373997854230-datalake-${env}/jobs/elasticmapreduce/" \
-  --name "Import task - ${env} ${release_id} ${study_id}" \
+  --name "Portal ETL - Fhavro Export - ${env} ${release_id} ${study_id}" \
   --instance-groups "[{\"InstanceCount\":${instance_count},\"InstanceGroupType\":\"CORE\",\"InstanceType\":\"${instance_type}\",\"Name\":\"Core - 2\"},{\"InstanceCount\":1,\"EbsConfiguration\":{\"EbsBlockDeviceConfigs\":[{\"VolumeSpecification\":{\"SizeInGB\":32,\"VolumeType\":\"gp2\"},\"VolumesPerInstance\":2}]},\"InstanceGroupType\":\"MASTER\",\"InstanceType\":\"m5.xlarge\",\"Name\":\"Master - 1\"}]" \
   --scale-down-behavior TERMINATE_AT_TASK_COMPLETION \
   --configurations file://./spark-config.json \
   --auto-terminate \
- --region us-east-1
+  --region us-east-1
