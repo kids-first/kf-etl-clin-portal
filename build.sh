1c44ad0fba97586897db30eeaf7da4410d8f2c8a
jecos <jcostanza@ferlab.bio>
2022-04-13 22:10:28 -0400

    build: Use latest version of testcontainer

--- build.sh
+++ build.sh
@@ -2,10 +2,8 @@
 set -e
 set -x
 
-mkdir -p ~/.ivy2 ~/.sbt ~/.m2 ~/.sbt_cache
 
-echo "Build docker image SBT+Docker use for running tests ..."
-docker build -t sbt-docker buildtools
+mkdir -p ~/.ivy2 ~/.sbt ~/.m2 ~/.sbt_cache
 
 echo "Cleanup project ..."
 docker run --net host --rm -v $(pwd):/app/project \
@@ -15,10 +13,11 @@ docker run --net host --rm -v $(pwd):/app/project \
     -v ~/.sbt:/app/.sbt \
     -v ~/.sbt_cache:/app/.cache \
     -w /app/project \
-    sbt-docker \
+    hseeberger/scala-sbt:11.0.14.1_1.6.2_2.12.15 \
     sbt -Duser.home=/app clean
 
 echo "Running tests ..."
+ls -al /var/run/docker.sock
 docker run --net host --rm -v $(pwd):/app/project \
     -v /var/run/docker.sock:/var/run/docker.sock \
     --user $(id -u):$(id -g) \
@@ -27,8 +26,8 @@ docker run --net host --rm -v $(pwd):/app/project \
     -v ~/.sbt:/app/.sbt \
     -v ~/.sbt_cache:/app/.cache \
     -w /app/project \
-    sbt-docker \
-    sbt -Duser.home=/app test
+    hseeberger/scala-sbt:11.0.14.1_1.6.2_2.12.15 \
+    sbt -Duser.home=/app fhavro_export/test
 
 echo "Generate config ..."
 docker run --net host --rm -v $(pwd):/app/project \
@@ -38,7 +37,7 @@ docker run --net host --rm -v $(pwd):/app/project \
     -v ~/.sbt:/app/.sbt \
     -v ~/.sbt_cache:/app/.cache \
     -w /app/project \
-    sbt-docker \
+    hseeberger/scala-sbt:11.0.14.1_1.6.2_2.12.15 \
     sbt -Duser.home=/app config/run
 
 echo "Build jars ..."
@@ -49,7 +48,7 @@ docker run --net host --rm -v $(pwd):/app/project \
     -v ~/.sbt:/app/.sbt \
     -v ~/.sbt_cache:/app/.cache \
     -w /app/project \
-    sbt-docker \
+    hseeberger/scala-sbt:11.0.14.1_1.6.2_2.12.15 \
     sbt -Duser.home=/app fhavro_export/assembly import_task/assembly prepare_index/assembly index_task/assembly publish_task/assembly
 
 echo "Build docker image fhavro-export:$git_commit"
