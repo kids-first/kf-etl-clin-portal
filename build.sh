82f934fd3d5c1a3d6d44de71345102e8d8396427
jecos <jcostanza@ferlab.bio>
2022-04-13 22:26:46 -0400

    build: try to remove user for others sbt step

--- build.sh
+++ build.sh
@@ -3,15 +3,14 @@ set -e
 set -x
 
 
-mkdir -p ~/.ivy2 ~/.sbt ~/.m2 ~/.sbt_cache
+mkdir -p ./.ivy2 ./.sbt ./.m2 ./.sbt_cache
 
 echo "Cleanup project ..."
 docker run --net host --rm -v $(pwd):/app/project \
-    --user $(id -u):$(id -g) \
-    -v ~/.m2:/app/.m2 \
-    -v ~/.ivy2:/app/.ivy2 \
-    -v ~/.sbt:/app/.sbt \
-    -v ~/.sbt_cache:/app/.cache \
+    -v ./.m2:/app/.m2 \
+    -v ./.ivy2:/app/.ivy2 \
+    -v ./.sbt:/app/.sbt \
+    -v ./.sbt_cache:/app/.cache \
     -w /app/project \
     hseeberger/scala-sbt:11.0.14.1_1.6.2_2.12.15 \
     sbt -Duser.home=/app clean
@@ -26,22 +25,20 @@ docker run --net host --rm -v $(pwd):/app/project \
 
 echo "Generate config ..."
 docker run --net host --rm -v $(pwd):/app/project \
-    --user $(id -u):$(id -g) \
-    -v ~/.m2:/app/.m2 \
-    -v ~/.ivy2:/app/.ivy2 \
-    -v ~/.sbt:/app/.sbt \
-    -v ~/.sbt_cache:/app/.cache \
+    -v ./.m2:/app/.m2 \
+    -v ./.ivy2:/app/.ivy2 \
+    -v ./.sbt:/app/.sbt \
+    -v ./.sbt_cache:/app/.cache \
     -w /app/project \
     hseeberger/scala-sbt:11.0.14.1_1.6.2_2.12.15 \
     sbt -Duser.home=/app config/run
 
 echo "Build jars ..."
 docker run --net host --rm -v $(pwd):/app/project \
-    --user $(id -u):$(id -g) \
-    -v ~/.m2:/app/.m2 \
-    -v ~/.ivy2:/app/.ivy2 \
-    -v ~/.sbt:/app/.sbt \
-    -v ~/.sbt_cache:/app/.cache \
+    -v ./.m2:/app/.m2 \
+    -v ./.ivy2:/app/.ivy2 \
+    -v ./.sbt:/app/.sbt \
+    -v ./.sbt_cache:/app/.cache \
     -w /app/project \
     hseeberger/scala-sbt:11.0.14.1_1.6.2_2.12.15 \
     sbt -Duser.home=/app fhavro_export/assembly import_task/assembly prepare_index/assembly index_task/assembly publish_task/assembly
