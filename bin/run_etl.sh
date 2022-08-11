f543e2175f871e79b2c7eed2a3489300e402e7b8
evans-g-crsj <eg.crsj@gmail.com>
2022-08-11 15:18:27 -0400

    :bug: Make param dynamic in Fhavro step (run_etl)

--- bin/run_etl.sh
+++ bin/run_etl.sh
@@ -132,7 +132,7 @@ STEPS=$(
     "Jar":"command-runner.jar",
     "Args":[
       "bash","-c",
-      "aws s3 cp s3://${BUCKET}/jobs/fhavro-export.jar /home/hadoop; cd /home/hadoop; /usr/lib/jvm/java-11-amazon-corretto.x86_64/bin/java -jar fhavro-export.jar ${RELEASE_ID} ${STUDIES} include-${ENV}"
+      "aws s3 cp s3://${BUCKET}/jobs/fhavro-export.jar /home/hadoop; cd /home/hadoop; /usr/lib/jvm/java-11-amazon-corretto.x86_64/bin/java -jar fhavro-export.jar ${RELEASE_ID} ${STUDIES} ${PROJECT}-${ENV}"
     ]
   },
 
