7e35938e720d6ced14ab3e6d5fd9dc57d2af04b7
adrian <adipaul1981@yahoo.ca>
2022-10-05 14:28:56 -0400

    update ES prd endpoint

--- bin/utils.sh
+++ bin/utils.sh
@@ -4,7 +4,7 @@ PROJECT_TO_NET_CONF["es_kf-strides_qa"]="https://vpc-kf-arranger-blue-es-service
 PROJECT_TO_NET_CONF["subnet_include_qa"]="subnet-0f1161ac2ee2fba5b"
 PROJECT_TO_NET_CONF["es_include_qa"]="https://vpc-include-arranger-blue-es-qa-xf3ttht4hjmxjfoh5u5x4jnw34.us-east-1.es.amazonaws.com"
 PROJECT_TO_NET_CONF["subnet_include_prd"]="subnet-0cdbe9ba6231146b5"
-PROJECT_TO_NET_CONF["es_include_prd"]="https://vpc-include-arranger-blue-es-qa-xf3ttht4hjmxjfoh5u5x4jnw34.us-east-1.es.amazonaws.com"
+PROJECT_TO_NET_CONF["es_include_prd"]="https://vpc-arranger-es-service-ykxirqamjqxyiyfg2rruxusfg4.us-east-1.es.amazonaws.com"
 
 check_project() {
   case $1 in
