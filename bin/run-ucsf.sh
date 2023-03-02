eeaf25f0575c612cfb8a976aa72c6f8a0d1fdc9a
Celine Pelletier <cpelletier@ferlab.bio>
2023-03-02 16:22:58 -0500

    :wrench: [WIP] script to run etl for ucsf

--- bin/run-ucsf.sh
+++ bin/run-ucsf.sh
@@ -1,82 +1,78 @@
 # Config
-release_id=${1}
-aws_profile=${2}
-study_id = "???" #FIXME
-kidsfirst_bucket="s3://kf-strides-232196027141-datalake-qa/jobs"
-es_endpoint= "???" #FIXME
+RELEASE_ID=${1}
+STUDY_ID="???" #FIXME
+KF_BUCKET="s3://kf-strides-232196027141-datalake-qa/jobs"
+ES_ENDPOINT="???" #FIXME
 
 # Copy jars from KF S3 bucket to here
-aws s3 cp ${kidsfirst_bucket}/fhavro-export.jar fhavro-export.jar
-aws s3 cp ${kidsfirst_bucket}/import-task.jar import-task.jar
-aws s3 cp ${kidsfirst_bucket}/prepare-index.jar prepare-index.jar
-aws s3 cp ${kidsfirst_bucket}/index-task.jar index-task.jar
-aws s3 cp ${kidsfirst_bucket}/publish-task.jar publish-task.jar
+aws s3 cp ${KF_BUCKET}/fhavro-export.jar fhavro-export.jar
+aws s3 cp ${KF_BUCKET}/import-task.jar import-task.jar
+aws s3 cp ${KF_BUCKET}/prepare-index.jar prepare-index.jar
+aws s3 cp ${KF_BUCKET}/index-task.jar index-task.jar
+aws s3 cp ${KF_BUCKET}/publish-task.jar publish-task.jar
 
 # Check spark conf are here
-[ ! -f $(pwd)/conf/spark-defaults.conf ] && exit 1
+if [[ ! -f $(pwd)/conf/spark-defaults.conf ]]; then
+    echo "Missing spark-defaults.conf file"
+    exit 1
+fi
 
 # Execute fhavro-export
-$JAVA_HOME/bin/java -jar fhavro-export.jar ${release_id} ${study_id} default
+docker run -it --rm -v $(pwd):/app amazoncorretto:11 java -jar /app/fhavro-export.jar ${RELEASE_ID} ${STUDY_ID} default
 
 # Execute import-task
 docker run -it --rm \
 -v $(pwd)/conf/spark-defaults.conf:/opt/spark/conf/spark-defaults.conf \
 -v $(pwd)/spark_ivy:/opt/spark/.ivy2 \
--v $HOME/.aws:/home/185/.aws \
--e AWS_PROFILE=${aws_profile} \
+-v $(pwd):/opt/spark/work-dir \
 -e AWS_REGION=us-east-1 \
 -p 4040:4040 apache/spark:3.3.1 \
-/opt/spark/bin/spark-submit --deploy-mode client --class bio.ferlab.fhir.etl.ImportTask import-task.jar config/ucsf.conf default ${release_id} ${study_id}
+/opt/spark/bin/spark-submit --deploy-mode client --class bio.ferlab.fhir.etl.ImportTask import-task.jar config/ucsf.conf default ${RELEASE_ID} ${STUDY_ID}
 
 # Execute prepare-index
 docker run -it --rm \
 -v $(pwd)/conf/spark-defaults.conf:/opt/spark/conf/spark-defaults.conf \
 -v $(pwd)/spark_ivy:/opt/spark/.ivy2 \
--v $HOME/.aws:/home/185/.aws \
--e AWS_PROFILE=${aws_profile} \
+-v $(pwd):/opt/spark/work-dir \
 -e AWS_REGION=us-east-1 \
 -p 4040:4040 apache/spark:3.3.1 \
-/opt/spark/bin/spark-submit --deploy-mode client --class bio.ferlab.fhir.etl.PrepareIndex prepare-index.jar config/ucsf.conf default all ${release_id} ${study_id}
+/opt/spark/bin/spark-submit --deploy-mode client --class bio.ferlab.fhir.etl.PrepareIndex prepare-index.jar config/ucsf.conf default all ${RELEASE_ID} ${STUDY_ID}
 
 # Execute index-task for study_centric
 docker run -it --rm \
 -v $(pwd)/conf/spark-defaults.conf:/opt/spark/conf/spark-defaults.conf \
 -v $(pwd)/spark_ivy:/opt/spark/.ivy2 \
--v $HOME/.aws:/home/185/.aws \
--e AWS_PROFILE=${aws_profile} \
+-v $(pwd):/opt/spark/work-dir \
 -e AWS_REGION=us-east-1 \
 -p 4040:4040 apache/spark:3.3.1 \
-/opt/spark/bin/spark-submit --deploy-mode client --class bio.ferlab.fhir.etl.IndexTask index-task.jar ${es_endpoint} 443 ${release_id} ${study_id} study_centric config/ucsf.conf
+/opt/spark/bin/spark-submit --deploy-mode client --class bio.ferlab.fhir.etl.IndexTask index-task.jar ${ES_ENDPOINT} 443 ${RELEASE_ID} ${STUDY_ID} study_centric config/ucsf.conf
 
 # Execute index-task for participant_centric
 docker run -it --rm \
 -v $(pwd)/conf/spark-defaults.conf:/opt/spark/conf/spark-defaults.conf \
 -v $(pwd)/spark_ivy:/opt/spark/.ivy2 \
--v $HOME/.aws:/home/185/.aws \
--e AWS_PROFILE=${aws_profile} \
+-v $(pwd):/opt/spark/work-dir \
 -e AWS_REGION=us-east-1 \
 -p 4040:4040 apache/spark:3.3.1 \
-/opt/spark/bin/spark-submit --deploy-mode client --class bio.ferlab.fhir.etl.IndexTask index-task.jar ${es_endpoint} 443 ${release_id} ${study_id} participant_centric config/ucsf.conf
+/opt/spark/bin/spark-submit --deploy-mode client --class bio.ferlab.fhir.etl.IndexTask index-task.jar ${ES_ENDPOINT} 443 ${RELEASE_ID} ${STUDY_ID} participant_centric config/ucsf.conf
 
 # Execute index-task for file_centric
 docker run -it --rm \
 -v $(pwd)/conf/spark-defaults.conf:/opt/spark/conf/spark-defaults.conf \
 -v $(pwd)/spark_ivy:/opt/spark/.ivy2 \
--v $HOME/.aws:/home/185/.aws \
--e AWS_PROFILE=${aws_profile} \
+-v $(pwd):/opt/spark/work-dir \
 -e AWS_REGION=us-east-1 \
 -p 4040:4040 apache/spark:3.3.1 \
-/opt/spark/bin/spark-submit --deploy-mode client --class bio.ferlab.fhir.etl.IndexTask index-task.jar ${es_endpoint} 443 ${release_id} ${study_id} file_centric config/ucsf.conf
+/opt/spark/bin/spark-submit --deploy-mode client --class bio.ferlab.fhir.etl.IndexTask index-task.jar ${ES_ENDPOINT} 443 ${RELEASE_ID} ${STUDY_ID} file_centric config/ucsf.conf
 
 # Execute index-task for biospecimen_centric
 docker run -it --rm \
 -v $(pwd)/conf/spark-defaults.conf:/opt/spark/conf/spark-defaults.conf \
 -v $(pwd)/spark_ivy:/opt/spark/.ivy2 \
--v $HOME/.aws:/home/185/.aws \
--e AWS_PROFILE=${aws_profile} \
+-v $(pwd):/opt/spark/work-dir \
 -e AWS_REGION=us-east-1 \
 -p 4040:4040 apache/spark:3.3.1 \
-/opt/spark/bin/spark-submit --deploy-mode client --class bio.ferlab.fhir.etl.IndexTask index-task.jar ${es_endpoint} 443 ${release_id} ${study_id} biospecimen_centric config/ucsf.conf
+/opt/spark/bin/spark-submit --deploy-mode client --class bio.ferlab.fhir.etl.IndexTask index-task.jar ${ES_ENDPOINT} 443 ${RELEASE_ID} ${STUDY_ID} biospecimen_centric config/ucsf.conf
 
 # Execute publish-task
-$JAVA_HOME/bin/java -jar publish-task.jar ${es_endpoint} 443 ${release_id} ${study_id} all
+docker run -it --rm -v $(pwd):/app amazoncorretto:11 java -jar /app/publish-task.jar ${ES_ENDPOINT} 443 ${RELEASE_ID} ${STUDY_ID} all
