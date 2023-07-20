61ad3e8ab24b7a0aaf8b47307bef75199194ff37
evans-g-crsj <eg.crsj@gmail.com>
2023-07-20 14:52:51 -0400

    :sparkles: Add roles information to family

--- enrich/src/test/scala/bio/ferlab/etl/enrich/model/FAMILY_ENRICHED.scala
+++ enrich/src/test/scala/bio/ferlab/etl/enrich/model/FAMILY_ENRICHED.scala
@@ -2,8 +2,7 @@ package bio.ferlab.etl.enrich.model
 
 case class FAMILY_ENRICHED(
                             family_fhir_id: String = "f",
-                            proband_participant_id: String = "p",
                             relations: Seq[RELATION] = Seq.empty,
                           )
 
-case class RELATION(`fhir_id`: String = "p", `role`: String = "father")
+case class RELATION(`participant_id`: String = "p", `role`: String = "father")
