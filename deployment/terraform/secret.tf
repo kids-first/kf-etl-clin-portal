fe2b942fd189dc25124c49d7b3af13c62ec6c332
Neel Kuila <neelk1123@gmail.com>
2023-08-14 16:53:11 -0400

    auto-save was off on vscode lol

---
+++ deployment/terraform/secret.tf
@@ -0,0 +1,13 @@
+resource "aws_secretsmanager_secret" "portal_etl_secret" {
+  name = "${var.project}/${var.environment}/secret"
+  description = "Portal ETL secrets"
+}
+
+resource "aws_secretsmanager_secret_version" "portal_etl_secret_version" {
+  secret_id     = aws_secretsmanager_secret.portal_etl_secret.id
+  secret_string = jsonencode(
+	{
+		"slack_webhook": ""
+	}
+  )
+}
