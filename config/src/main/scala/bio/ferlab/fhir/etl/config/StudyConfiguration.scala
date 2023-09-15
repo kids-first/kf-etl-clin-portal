1e38d19aa294901e4ba3aaa71fcde03e5cfc860a
jecos <jcostanza@ferlab.bio>
2023-09-15 13:45:51 -0400

    :bug: Fix study config

--- config/src/main/scala/bio/ferlab/fhir/etl/config/StudyConfiguration.scala
+++ config/src/main/scala/bio/ferlab/fhir/etl/config/StudyConfiguration.scala
@@ -14,6 +14,33 @@ object StudyConfiguration {
   )
 
   private val kfStudiesConfiguration = Map(
+    "SD_46SK55A3" ->StudyConfiguration(
+      snvVCFPattern = ".*/harmonized/family-variants/.*filtered.deNovo.vep.vcf.gz"
+    ),
+    "SD_9PYZAHHE" -> StudyConfiguration(
+      snvVCFPattern = ".*/harmonized/family-variants/.*filtered.deNovo.vep.vcf.gz"
+    ),
+    "SD_DYPMEHHF" -> StudyConfiguration(
+      snvVCFPattern = ".*/harmonized/family-variants/.*filtered.deNovo.vep.vcf.gz"
+    ),
+    "SD_6FPYJQBR" -> StudyConfiguration(
+      snvVCFPattern = ".*/harmonized/family-variants/.*filtered.deNovo.vep.vcf.gz"
+    ),
+    "SD_YGVA0E1C" -> StudyConfiguration(
+      snvVCFPattern = ".*/harmonized/family-variants/.*filtered.deNovo.vep.vcf.gz"
+    ),
+    "SD_R0EPRSGS" -> StudyConfiguration(
+      snvVCFPattern = ".*/harmonized/family-variants/.*filtered.deNovo.vep.vcf.gz"
+    ),
+    "SD_B8X3C1MX" -> StudyConfiguration(
+      snvVCFPattern = ".*/harmonized/family-variants/.*filtered.deNovo.vep.vcf.gz"
+    ),
+    "SD_DK0KRWK8" -> StudyConfiguration(
+      snvVCFPattern = ".*/harmonized/family-variants/.*filtered.deNovo.vep.vcf.gz"
+    ),
+    "SD_PET7Q6F2" -> StudyConfiguration(
+      snvVCFPattern = ".*/harmonized/simple-variants/.*filtered.deNovo.vep.vcf.gz"
+    ),
     "SD_BHJXBDQK" -> StudyConfiguration(
       snvVCFPattern = ".*/harmonized-data/simple-variants/.*filtered.*vep.vcf.gz"
     ),
