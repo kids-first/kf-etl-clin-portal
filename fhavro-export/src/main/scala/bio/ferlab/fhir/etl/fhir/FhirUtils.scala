119ae76a70ed126998cc6ee82d8b040f8de4c42c
evans-g-crsj <eg.crsj@gmail.com>
2023-09-27 11:54:35 -0400

    :mag: Double default socket timeout for fhir client

--- fhavro-export/src/main/scala/bio/ferlab/fhir/etl/fhir/FhirUtils.scala
+++ fhavro-export/src/main/scala/bio/ferlab/fhir/etl/fhir/FhirUtils.scala
@@ -3,6 +3,7 @@ package bio.ferlab.fhir.etl.fhir
 import bio.ferlab.fhir.etl.auth.CookieInterceptor
 import bio.ferlab.fhir.etl.config.Config
 import ca.uhn.fhir.context.FhirContext
+import ca.uhn.fhir.rest.client.api.IRestfulClientFactory
 import ca.uhn.fhir.rest.client.impl.GenericClient
 import ca.uhn.fhir.rest.client.interceptor.LoggingInterceptor
 import org.apache.http.HttpHost
@@ -16,7 +17,10 @@ object FhirUtils {
 
   def buildFhirClient(config: Config, verbose: Boolean): GenericClient = {
     val loggingInterceptor = new LoggingInterceptor()
-    val fhirClient: GenericClient = fhirContext.getRestfulClientFactory.newGenericClient(s"${config.fhirConfig.baseUrl}").asInstanceOf[GenericClient]
+
+    val ctx = fhirContext.getRestfulClientFactory
+    ctx.setSocketTimeout(2 * IRestfulClientFactory.DEFAULT_SOCKET_TIMEOUT)
+    val fhirClient: GenericClient = ctx.newGenericClient(s"${config.fhirConfig.baseUrl}").asInstanceOf[GenericClient]
     if (verbose) {
       loggingInterceptor.setLogRequestSummary(true)
       loggingInterceptor.setLogRequestBody(true)
