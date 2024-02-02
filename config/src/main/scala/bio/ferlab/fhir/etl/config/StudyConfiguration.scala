dcd024c8a98b520cda623a1b50c1b994dd2c1f61
Celine Pelletier <cpelletier@ferlab.bio>
2024-02-02 09:03:58 -0500

    feat: SKFP-793 fix study configuration to match excel file

--- config/src/main/scala/bio/ferlab/fhir/etl/config/StudyConfiguration.scala
+++ config/src/main/scala/bio/ferlab/fhir/etl/config/StudyConfiguration.scala
@@ -14,41 +14,52 @@ object StudyConfiguration {
   )
 
   val kfStudiesConfiguration: Map[String, StudyConfiguration] = Map(
-    "SD_54G4WG4R" -> StudyConfiguration(
-      snvVCFPattern = "(.*/harmonized-data/family-variants/.*.CGP.filtered.deNovo.vep.vcf.gz|.*/harmonized-data/family-variants/.*.multi.vqsr.filtered.denovo.vep_105.vcf.gz)"
+    "SD_RM8AFW0R" -> StudyConfiguration(
+      snvVCFPattern = ".*/harmonized-data/family-variants/.*.CGP.filtered.deNovo.vep.vcf.gz"
+    ),
+    "SD_Z0D9N23X" -> StudyConfiguration(
+      snvVCFPattern = ".*/harmonized-data/family-variants/.*.CGP.filtered.deNovo.vep.vcf.gz"
     ),
     "SD_46SK55A3" -> StudyConfiguration(
       snvVCFPattern = "(.*/harmonized/family-variants/.*.CGP.filtered.deNovo.vep.vcf.gz|.*/harmonized/family-variants/.*.postCGP.filtered.deNovo.vep.vcf.gz)"
     ),
-    "SD_9PYZAHHE" -> StudyConfiguration(
-      snvVCFPattern = "(.*/harmonized/family-variants/.*.CGP.filtered.deNovo.vep.vcf.gz)"
+    "SD_2CEKQ05V" -> StudyConfiguration(
+      snvVCFPattern = "(.*/harmonized-data/family-variants/.*.postCGP.filtered.deNovo.vep.vcf.gz)"
     ),
-    "SD_DYPMEHHF" -> StudyConfiguration(
-      snvVCFPattern = "(.*/harmonized-data/family-variants/.*.multi.vqsr.filtered.denovo.vep_105.vcf.gz)"
+    "SD_54G4WG4R" -> StudyConfiguration(
+      snvVCFPattern = "(.*/harmonized-data/family-variants/.*.CGP.filtered.deNovo.vep.vcf.gz|.*/harmonized-data/family-variants/.*.multi.vqsr.filtered.denovo.vep_105.vcf.gz)"
+    ),
+    "SD_PREASA7S" -> StudyConfiguration(
+      snvVCFPattern = "(" +
+        ".*/harmonized/family-variants/.*.CGP.filtered.deNovo.vep.vcf.gz" + "|" +
+        ".*/harmonized/family-variants/.*.postCGP.filtered.deNovo.vep.vcf.gz" + "|" +
+        ".*/harmonized-data/family-variants/.*.CGP.filtered.deNovo.vep.vcf.gz" + "|" +
+        ".*/harmonized-data/family-variants/.*.postCGP.filtered.deNovo.vep.vcf.gz" +
+        ")"
+    ),
+    "SD_Z6MWD3H0" -> StudyConfiguration(
+      snvVCFPattern = "(.*/harmonized-data/family-variants/.*.CGP.filtered.deNovo.vep.vcf.gz|.*/harmonized-data/family-variants/.*.multi.vqsr.filtered.denovo.vep_105.vcf.gz)"
     ),
     "SD_6FPYJQBR" -> StudyConfiguration(
       snvVCFPattern = "(.*/harmonized/family-variants/.*.CGP.filtered.deNovo.vep.vcf.gz)"
     ),
+    "SD_0TYVY1TW" -> StudyConfiguration(
+      snvVCFPattern = "(.*/harmonized-data/family-variants/.*.CGP.filtered.deNovo.vep.vcf.gz)"
+    ),
     "SD_YGVA0E1C" -> StudyConfiguration(
       snvVCFPattern = "(.*/harmonized/family-variants/.*.CGP.filtered.deNovo.vep.vcf.gz|.*/harmonized/family-variants/.*.postCGP.filtered.deNovo.vep.vcf.gz)"
     ),
-    "SD_R0EPRSGS" -> StudyConfiguration(
-      snvVCFPattern = "(.*/harmonized/family-variants/.*.CGP.filtered.deNovo.vep.vcf.gz)"
+    "SD_W0V965XZ" -> StudyConfiguration(
+      snvVCFPattern = "(.*/harmonized-data/family-variants/.*.CGP.filtered.deNovo.vep.vcf.gz)"
     ),
-    "SD_B8X3C1MX" -> StudyConfiguration(
-      snvVCFPattern = ".*/harmonized/family-variants/.*filtered.deNovo.vep.vcf.gz"
+    "SD_GPZG67FZ" -> StudyConfiguration(
+      snvVCFPattern = "(.*/harmonized-data/family-variants/.*.CGP.filtered.deNovo.vep.vcf.gz)"
     ),
-    "SD_DK0KRWK8" -> StudyConfiguration(
-      snvVCFPattern = ".*/harmonized/family-variants/.*filtered.deNovo.vep.vcf.gz"
+    "SD_DZ4GPQX6" -> StudyConfiguration(
+      snvVCFPattern = "(.*/harmonized-data/family-variants/.*.CGP.filtered.deNovo.vep.vcf.gz)"
     ),
-    "SD_PET7Q6F2" -> StudyConfiguration(
-      snvVCFPattern = ".*/harmonized/simple-variants/.*filtered.deNovo.vep.vcf.gz"
-    ),
-    "SD_BHJXBDQK" -> StudyConfiguration(
-      snvVCFPattern = "(.*/harmonized-data/simple-variants/.*.CGP.filtered.vep.vcf.gz|.*/harmonized-data/simple-variants/.*.multi.vqsr.filtered.denovo.vep_105.vcf.gz)"
-    ),
-    "SD_ZXJFFMEF" -> StudyConfiguration(
-      snvVCFPattern = "(.*/harmonized-data/simple-variants/.*.CGP.filtered.deNovo.vep.vcf.gz)"
+    "SD_JWS3V24D" -> StudyConfiguration(
+      snvVCFPattern = "(.*/harmonized-data/family-variants/.*.postCGP.filtered.deNovo.vep.vcf.gz)"
     ),
     "SD_QBG7P5P7" -> StudyConfiguration(
       snvVCFPattern = "(.*/harmonized-data/family-variants/.*.CGP.filtered.deNovo.vep.vcf.gz)"
@@ -62,20 +73,43 @@ object StudyConfiguration {
     "SD_JK4Z4T6V" -> StudyConfiguration(
       snvVCFPattern = "(.*/harmonized-data/family-variants/.*.CGP.filtered.deNovo.vep.vcf.gz)"
     ),
+    "SD_PET7Q6F2" -> StudyConfiguration(
+      snvVCFPattern = ".*/harmonized-data/simple-variants/.*filtered.deNovo.vep.vcf.gz"
+    ),
+    "SD_DYPMEHHF" -> StudyConfiguration(
+      snvVCFPattern = "(.*/harmonized-data/family-variants/.*.multi.vqsr.filtered.denovo.vep_105.vcf.gz)"
+    ),
     "SD_P445ACHV" -> StudyConfiguration(
       snvVCFPattern = "(.*/harmonized-data/family-variants/.*.CGP.filtered.deNovo.vep.vcf.gz)"
     ),
     "SD_15A2MQQ9" -> StudyConfiguration(
       snvVCFPattern = "(.*/harmonized-data/family-variants/.*.CGP.filtered.deNovo.vep.vcf.gz)"
     ),
+    "SD_9PYZAHHE" -> StudyConfiguration(
+      snvVCFPattern = "(.*/harmonized/family-variants/.*.CGP.filtered.deNovo.vep.vcf.gz)"
+    ),
+    "SD_R0EPRSGS" -> StudyConfiguration(
+      snvVCFPattern = "(.*/harmonized/family-variants/.*.CGP.filtered.deNovo.vep.vcf.gz)"
+    ),
     "SD_W6FWTD8A" -> StudyConfiguration(
       snvVCFPattern = "(.*/harmonized-data/family-variants/.*.CGP.filtered.deNovo.vep.vcf.gz)"
+    ),
+    "SD_ZXJFFMEF" -> StudyConfiguration(
+      snvVCFPattern = "(.*/harmonized-data/simple-variants/.*.CGP.filtered.deNovo.vep.vcf.gz)"
     ),
     "SD_DZTB5HRR" -> StudyConfiguration(
       snvVCFPattern = "(.*/harmonized/family-variants/.*.CGP.filtered.deNovo.vep.vcf.gz)"
     ),
     "SD_AQ9KVN5P" -> StudyConfiguration(
       snvVCFPattern = "(.*/harmonized-data/simple-variants/.*.CGP.filtered.deNovo.vep.vcf.gz|.*/harmonized-data/simple-variants/.*.postCGP.Gfiltered.vcf.gz)"
+    ),
+    "SD_BHJXBDQK" -> StudyConfiguration(
+      snvVCFPattern = "(" +
+        ".*/harmonized-data/simple-variants/.*.CGP.filtered.vep.vcf.gz" + "|" +
+        ".*/harmonized-data/family-variants/.*.CGP.filtered.vep.vcf.gz" + "|" +
+        ".*/harmonized-data/simple-variants/.*.multi.vqsr.filtered.denovo.vep_105.vcf.gz" +
+        ".*/harmonized-data/family-variants/.*.multi.vqsr.filtered.denovo.vep_105.vcf.gz" +
+        ")"
     ),
     "SD_8Y99QZJJ" -> StudyConfiguration(
       snvVCFPattern = "(.*/harmonized-data/simple-variants/.*.CGP.filtered.deNovo.vep.vcf.gz)"
--- config/src/test/scala/bio/ferlab/fhir/etl/config/StudyConfigurationSpec.scala
+++ config/src/test/scala/bio/ferlab/fhir/etl/config/StudyConfigurationSpec.scala
@@ -12,9 +12,6 @@ class StudyConfigurationSpec extends AnyFlatSpec with Matchers with WithSparkSes
 
   import spark.implicits._
 
-  // No real example was found - will not be tested :(
-  val NO_FILE_PATH_EXAMPLE_FOUND: Seq[String] = Seq.empty
-
   // "data" represents pertinent information found in given excel where patterns are given.
   val data: Seq[Datum] = Seq(
     Datum(
@@ -22,6 +19,35 @@ class StudyConfigurationSpec extends AnyFlatSpec with Matchers with WithSparkSes
       pattern = defaultStudyConfiguration.snvVCFPattern,
       paths = Seq(
         "s3://kf-study-us-east-1-prd-sd-no-exists-in-conf/harmonized-data/family-variants/e791460c-92df-46d9-83b5-4f49e4223238.postCGP.filtered.deNovo.vep.vcf.gz"
+      )
+    ),
+    Datum(
+      studyId = "SD_RM8AFW0R",
+      pattern = kfStudiesConfiguration("SD_RM8AFW0R").snvVCFPattern,
+      paths = Seq(
+        "s3://kf-study-us-east-1-prd-sd-rm8afw0r/harmonized-data/family-variants/e791460c-92df-46d9-83b5-4f49e4223238.CGP.filtered.deNovo.vep.vcf.gz"
+      )
+    ),
+    Datum(
+      studyId = "SD_Z0D9N23X",
+      pattern = kfStudiesConfiguration("SD_Z0D9N23X").snvVCFPattern,
+      paths = Seq(
+        "s3://kf-study-us-east-1-prd-sd-z0d9n23x/harmonized-data/family-variants/e791460c-92df-46d9-83b5-4f49e4223238.CGP.filtered.deNovo.vep.vcf.gz"
+      )
+    ),
+    Datum(
+      studyId = "SD_46SK55A3",
+      pattern = kfStudiesConfiguration("SD_46SK55A3").snvVCFPattern,
+      paths = Seq(
+        "s3://kf-study-us-east-1-prd-sd-46sk55a3/harmonized/family-variants/e791460c-92df-46d9-83b5-4f49e4223238.CGP.filtered.deNovo.vep.vcf.gz",
+        "s3://kf-study-us-east-1-prd-sd-46sk55a3/harmonized/family-variants/e791460c-92df-46d9-83b5-4f49e4223238.postCGP.filtered.deNovo.vep.vcf.gz"
+      )
+    ),
+    Datum(
+      studyId = "SD_2CEKQ05V",
+      pattern = kfStudiesConfiguration("SD_2CEKQ05V").snvVCFPattern,
+      paths = Seq(
+        "s3://kf-study-us-east-1-prd-sd-2cekq05v/harmonized-data/family-variants/e791460c-92df-46d9-83b5-4f49e4223238.postCGP.filtered.deNovo.vep.vcf.gz",
       )
     ),
     Datum(
@@ -33,41 +59,75 @@ class StudyConfigurationSpec extends AnyFlatSpec with Matchers with WithSparkSes
       )
     ),
     Datum(
-      studyId = "SD_PET7Q6F2",
-      pattern = kfStudiesConfiguration("SD_PET7Q6F2").snvVCFPattern,
+      studyId = "SD_PREASA7S",
+      pattern = kfStudiesConfiguration("SD_PREASA7S").snvVCFPattern,
       paths = Seq(
-        "s3://kf-study-us-east-1-prd-sd-pet7q6f2/harmonized-data/simple-variants/a9dab2bd-3753-420b-b7b8-a845ac06bfe5.CGP.filtered.deNovo.vep.vcf.gz"
+        "s3://kf-study-us-east-1-prd-sd-preasa7s/harmonized/family-variants/347dea75-c8a1-47da-87de-16b57c5aa3cd.CGP.filtered.deNovo.vep.vcf.gz",
+        "s3://kf-study-us-east-1-prd-sd-preasa7s/harmonized/family-variants/347dea75-c8a1-47da-87de-16b57c5aa3cd.postCGP.filtered.deNovo.vep.vcf.gz",
+        "s3://kf-study-us-east-1-prd-sd-preasa7s/harmonized-data/family-variants/37fff1d3-9389-4a35-87a0-e554e2a63e1d.CGP.filtered.deNovo.vep.vcf.gz",
+        "s3://kf-study-us-east-1-prd-sd-preasa7s/harmonized-data/family-variants/37fff1d3-9389-4a35-87a0-e554e2a63e1d.postCGP.filtered.deNovo.vep.vcf.gz"
       )
     ),
     Datum(
-      studyId = "SD_DYPMEHHF",
-      pattern = kfStudiesConfiguration("SD_DYPMEHHF").snvVCFPattern,
-      paths = NO_FILE_PATH_EXAMPLE_FOUND
+      studyId = "SD_Z6MWD3H0",
+      pattern = kfStudiesConfiguration("SD_Z6MWD3H0").snvVCFPattern,
+      paths = Seq(
+        "s3://kf-study-us-east-1-prd-sd-z6mwd3h0/harmonized-data/family-variants/a9dab2bd-3753-420b-b7b8-a845ac06bfe5.CGP.filtered.deNovo.vep.vcf.gz",
+        "s3://kf-study-us-east-1-prd-sd-z6mwd3h0/harmonized-data/family-variants/a9dab2bd-3753-420b-b7b8-a845ac06bfe5.multi.vqsr.filtered.denovo.vep_105.vcf.gz",
+      )
     ),
     Datum(
-      studyId = "SD_9PYZAHHE",
-      pattern = kfStudiesConfiguration("SD_9PYZAHHE").snvVCFPattern,
-      paths = Seq("s3://kf-study-us-east-1-prd-sd-9pyzahhe/harmonized/family-variants/cc357ef5-27b7-4af0-b106-dfc8d932f22b.CGP.filtered.deNovo.vep.vcf.gz")
+      studyId = "SD_6FPYJQBR",
+      pattern = kfStudiesConfiguration("SD_6FPYJQBR").snvVCFPattern,
+      paths = Seq("s3://kf-study-us-east-1-prd-sd-6fpyjqbr/harmonized/family-variants/9bacad7b-b55a-4b13-8b92-88c90f47f0e1.CGP.filtered.deNovo.vep.vcf.gz")
     ),
     Datum(
-      studyId = "SD_R0EPRSGS",
-      pattern = kfStudiesConfiguration("SD_R0EPRSGS").snvVCFPattern,
-      paths = Seq("s3://kf-study-us-east-1-prd-sd-r0eprsgs/harmonized/family-variants/8ade709c-af25-4e94-aba4-19e1a0511dca.CGP.filtered.deNovo.vep.vcf.gz")
+      studyId = "SD_0TYVY1TW",
+      pattern = kfStudiesConfiguration("SD_0TYVY1TW").snvVCFPattern,
+      paths = Seq("s3://kf-study-us-east-1-prd-sd-0tyvy1tw/harmonized-data/family-variants/9bacad7b-b55a-4b13-8b92-88c90f47f0e1.CGP.filtered.deNovo.vep.vcf.gz")
     ),
     Datum(
-      studyId = "SD_ZXJFFMEF",
-      pattern = kfStudiesConfiguration("SD_ZXJFFMEF").snvVCFPattern,
-      paths = Seq("s3://kf-study-us-east-1-prd-sd-zxjffmef/harmonized-data/simple-variants/c3556efe-6ccd-4a40-85ac-1eab04b7b575.CGP.filtered.deNovo.vep.vcf.gz")
+      studyId = "SD_YGVA0E1C",
+      pattern = kfStudiesConfiguration("SD_YGVA0E1C").snvVCFPattern,
+      paths = Seq(
+        "s3://kf-study-us-east-1-prd-sd-ygva0e1c/harmonized/family-variants/9bacad7b-b55a-4b13-8b92-88c90f47f0e1.CGP.filtered.deNovo.vep.vcf.gz",
+        "s3://kf-study-us-east-1-prd-sd-ygva0e1c/harmonized/family-variants/9bacad7b-b55a-4b13-8b92-88c90f47f0e1.postCGP.filtered.deNovo.vep.vcf.gz"
+      )
     ),
     Datum(
-      studyId = "SD_BHJXBDQK",
-      pattern = kfStudiesConfiguration("SD_BHJXBDQK").snvVCFPattern,
-      paths = NO_FILE_PATH_EXAMPLE_FOUND
+      studyId = "SD_W0V965XZ",
+      pattern = kfStudiesConfiguration("SD_W0V965XZ").snvVCFPattern,
+      paths = Seq(
+        "s3://kf-study-us-east-1-prd-sd-w0v965xz/harmonized-data/family-variants/9bacad7b-b55a-4b13-8b92-88c90f47f0e1.CGP.filtered.deNovo.vep.vcf.gz",
+      )
+    ),
+    Datum(
+      studyId = "SD_GPZG67FZ",
+      pattern = kfStudiesConfiguration("SD_GPZG67FZ").snvVCFPattern,
+      paths = Seq(
+        "s3://kf-strides-study-us-east-1-prd-sd-gpzg67fz/harmonized-data/family-variants/9bacad7b-b55a-4b13-8b92-88c90f47f0e1.CGP.filtered.deNovo.vep.vcf.gz",
+      )
+    ),
+    Datum(
+      studyId = "SD_DZ4GPQX6",
+      pattern = kfStudiesConfiguration("SD_DZ4GPQX6").snvVCFPattern,
+      paths = Seq(
+        "s3://kf-study-us-east-1-prd-sd-dz4gpqx6/harmonized-data/family-variants/9bacad7b-b55a-4b13-8b92-88c90f47f0e1.CGP.filtered.deNovo.vep.vcf.gz",
+      )
+    ),
+    Datum(
+      studyId = "SD_JWS3V24D",
+      pattern = kfStudiesConfiguration("SD_JWS3V24D").snvVCFPattern,
+      paths = Seq(
+        "s3://kf-study-us-east-1-prd-sd-jws3v24d/harmonized-data/family-variants/9bacad7b-b55a-4b13-8b92-88c90f47f0e1.postCGP.filtered.deNovo.vep.vcf.gz",
+      )
     ),
     Datum(
       studyId = "SD_QBG7P5P7",
       pattern = kfStudiesConfiguration("SD_QBG7P5P7").snvVCFPattern,
-      paths = NO_FILE_PATH_EXAMPLE_FOUND
+      paths = Seq(
+        "s3://kf-study-us-east-1-prd-sd-qbg7p5p7/harmonized-data/family-variants/9bacad7b-b55a-4b13-8b92-88c90f47f0e1.CGP.filtered.deNovo.vep.vcf.gz",
+      )
     ),
     Datum(
       studyId = "SD_VTTSHWV4",
@@ -90,9 +150,25 @@ class StudyConfigurationSpec extends AnyFlatSpec with Matchers with WithSparkSes
       )
     ),
     Datum(
+      studyId = "SD_PET7Q6F2",
+      pattern = kfStudiesConfiguration("SD_PET7Q6F2").snvVCFPattern,
+      paths = Seq(
+        "s3://kf-study-us-east-1-prd-sd-pet7q6f2/harmonized-data/simple-variants/a9dab2bd-3753-420b-b7b8-a845ac06bfe5.CGP.filtered.deNovo.vep.vcf.gz"
+      )
+    ),
+    Datum(
+      studyId = "SD_DYPMEHHF",
+      pattern = kfStudiesConfiguration("SD_DYPMEHHF").snvVCFPattern,
+      paths = Seq(
+        "s3://kf-study-us-east-1-prd-sd-dypmehhf/harmonized-data/family-variants/a9dab2bd-3753-420b-b7b8-a845ac06bfe5.multi.vqsr.filtered.denovo.vep_105.vcf.gz"
+      )
+    ),
+    Datum(
       studyId = "SD_P445ACHV",
       pattern = kfStudiesConfiguration("SD_P445ACHV").snvVCFPattern,
-      paths = NO_FILE_PATH_EXAMPLE_FOUND
+      paths = Seq(
+        "s3://kf-study-us-east-1-prd-sd-p445achv/harmonized-data/family-variants/a9dab2bd-3753-420b-b7b8-a845ac06bfe5.CGP.filtered.deNovo.vep.vcf.gz"
+      )
     ),
     Datum(
       studyId = "SD_15A2MQQ9",
@@ -100,59 +176,64 @@ class StudyConfigurationSpec extends AnyFlatSpec with Matchers with WithSparkSes
       paths = Seq("s3://kf-strides-study-us-east-1-prd-sd-15a2mqq9/harmonized-data/family-variants/447d9f46-9328-4bae-a39d-ac29d8a0c197.CGP.filtered.deNovo.vep.vcf.gz")
     ),
     Datum(
+      studyId = "SD_9PYZAHHE",
+      pattern = kfStudiesConfiguration("SD_9PYZAHHE").snvVCFPattern,
+      paths = Seq("s3://kf-study-us-east-1-prd-sd-9pyzahhe/harmonized/family-variants/cc357ef5-27b7-4af0-b106-dfc8d932f22b.CGP.filtered.deNovo.vep.vcf.gz")
+    ),
+    Datum(
+      studyId = "SD_R0EPRSGS",
+      pattern = kfStudiesConfiguration("SD_R0EPRSGS").snvVCFPattern,
+      paths = Seq("s3://kf-study-us-east-1-prd-sd-r0eprsgs/harmonized/family-variants/8ade709c-af25-4e94-aba4-19e1a0511dca.CGP.filtered.deNovo.vep.vcf.gz")
+    ),
+    Datum(
       studyId = "SD_W6FWTD8A",
       pattern = kfStudiesConfiguration("SD_W6FWTD8A").snvVCFPattern,
       paths = Seq("s3://kf-strides-study-us-east-1-prd-sd-w6fwtd8a/harmonized-data/family-variants/5ec00e64-8aa0-45b6-8012-272647201f21.CGP.filtered.deNovo.vep.vcf.gz")
     ),
     Datum(
+      studyId = "SD_ZXJFFMEF",
+      pattern = kfStudiesConfiguration("SD_ZXJFFMEF").snvVCFPattern,
+      paths = Seq("s3://kf-study-us-east-1-prd-sd-zxjffmef/harmonized-data/simple-variants/c3556efe-6ccd-4a40-85ac-1eab04b7b575.CGP.filtered.deNovo.vep.vcf.gz")
+    ),
+    Datum(
       studyId = "SD_DZTB5HRR",
       pattern = kfStudiesConfiguration("SD_DZTB5HRR").snvVCFPattern,
-      paths = NO_FILE_PATH_EXAMPLE_FOUND
+      paths = Seq(
+        "s3://kf-study-us-east-1-prd-sd-dztb5hrr/harmonized/family-variants/a9dab2bd-3753-420b-b7b8-a845ac06bfe5.CGP.filtered.deNovo.vep.vcf.gz"
+      )
     ),
     Datum(
       studyId = "SD_AQ9KVN5P",
       pattern = kfStudiesConfiguration("SD_AQ9KVN5P").snvVCFPattern,
-      paths = NO_FILE_PATH_EXAMPLE_FOUND
+      paths = Seq(
+        "s3://kf-strides-study-us-east-1-prd-sd-aq9kvn5p/harmonized-data/simple-variants/a9dab2bd-3753-420b-b7b8-a845ac06bfe5.CGP.filtered.deNovo.vep.vcf.gz",
+        "s3://kf-strides-study-us-east-1-prd-sd-aq9kvn5p/harmonized-data/simple-variants/a9dab2bd-3753-420b-b7b8-a845ac06bfe5.postCGP.Gfiltered.vcf.gz"
+      )
+    ),
+    Datum(
+      studyId = "SD_BHJXBDQK",
+      pattern = kfStudiesConfiguration("SD_BHJXBDQK").snvVCFPattern,
+      paths = Seq(
+        "s3://cds-246-phs002517-sequencefiles-p30-fy20/kf-study-us-east-1-prd-sd-bhjxbdqk/harmonized-data/simple-variants/a9dab2bd-3753-420b-b7b8-a845ac06bfe5.CGP.filtered.vep.vcf.gz",
+        "s3://cds-246-phs002517-sequencefiles-p30-fy20/kf-study-us-east-1-prd-sd-bhjxbdqk/harmonized-data/simple-variants/a9dab2bd-3753-420b-b7b8-a845ac06bfe5.multi.vqsr.filtered.denovo.vep_105.vcf.gz",
+        "s3://cds-306-phs002517-x01/harmonized-data/family-variants/a9dab2bd-3753-420b-b7b8-a845ac06bfe5.CGP.filtered.vep.vcf.gz",
+        "s3://cds-306-phs002517-x01/harmonized-data/family-variants/a9dab2bd-3753-420b-b7b8-a845ac06bfe5.multi.vqsr.filtered.denovo.vep_105.vcf.gz"
+      )
     ),
     Datum(
       studyId = "SD_8Y99QZJJ",
       pattern = kfStudiesConfiguration("SD_8Y99QZJJ").snvVCFPattern,
-      paths = NO_FILE_PATH_EXAMPLE_FOUND
+      paths = Seq(
+        "s3://cds-246-phs002517-sequencefiles-p30-fy20/kf-study-us-east-1-prd-sd-8y99qzjj/harmonized-data/simple-variants/a9dab2bd-3753-420b-b7b8-a845ac06bfe5.CGP.filtered.deNovo.vep.vcf.gz",
+      )
     ),
-    Datum(
-      studyId = "SD_46SK55A3",
-      pattern = kfStudiesConfiguration("SD_46SK55A3").snvVCFPattern,
-      paths = NO_FILE_PATH_EXAMPLE_FOUND
-    ),
-    Datum(
-      studyId = "SD_6FPYJQBR",
-      pattern = kfStudiesConfiguration("SD_6FPYJQBR").snvVCFPattern,
-      paths = Seq("s3://kf-study-us-east-1-prd-sd-6fpyjqbr/harmonized/family-variants/9bacad7b-b55a-4b13-8b92-88c90f47f0e1.CGP.filtered.deNovo.vep.vcf.gz")
-    ),
-    Datum(
-      studyId = "SD_YGVA0E1C",
-      pattern = kfStudiesConfiguration("SD_YGVA0E1C").snvVCFPattern,
-      paths = NO_FILE_PATH_EXAMPLE_FOUND
-    ),
-    Datum( // not found in xslx
-      studyId = "SD_B8X3C1MX",
-      pattern = kfStudiesConfiguration("SD_B8X3C1MX").snvVCFPattern,
-      paths = NO_FILE_PATH_EXAMPLE_FOUND
-    ),
-    Datum( // not found in xslx
-      studyId = "SD_DK0KRWK8",
-      pattern = kfStudiesConfiguration("SD_DK0KRWK8").snvVCFPattern,
-      paths = NO_FILE_PATH_EXAMPLE_FOUND
-    )
   )
 
-  val dataStudyIds: Seq[String] = data.map(x => x.studyId)
+  val dataStudyIds: Set[String] = data.map(x => x.studyId).toSet
   // ===== Sanity checks ===== //
-  dataStudyIds.toSet.size shouldEqual data.length
+  dataStudyIds.size shouldEqual data.length
   data.count(x => {
-    val reshapedStudyId = x.studyId.toLowerCase.replaceAll("_", "-")
-    val pathsExamplesSeemAkinToStudy = x.paths.forall(p => p.contains(reshapedStudyId))
-    x.paths.nonEmpty && pathsExamplesSeemAkinToStudy
+    x.paths.nonEmpty
   }) shouldEqual data.count(x => x.paths.nonEmpty)
   kfStudiesConfiguration.keys.forall(k => dataStudyIds.contains(k)) shouldBe true
   // ===== //
