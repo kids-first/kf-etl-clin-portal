bb66f7901e57a049aae7d51020c3451c4d10a6fb
haynescd <haynescd@users.noreply.github.com>
2024-03-20 14:31:14 +0000

    🎨 Apply terraform fmt

--- deployment/terraform/lambda.tf
+++ deployment/terraform/lambda.tf
@@ -141,10 +141,10 @@ data "archive_file" "archive_add_portal_etl_emr_step_lambda" {
 }
 
 resource "aws_lambda_function" "add_portal_etl_emr_step_lambda" {
-  function_name    = "PortalEtl-Submit-ETL-Step-${var.environment}"
-  filename         = "${path.module}/../lambda_functions/add_portal_etl_emr_step_archive.zip"
-  role             = aws_iam_role.lambda_service_role.arn
-  handler          = "main.add_portal_etl_emr_step"
+  function_name = "PortalEtl-Submit-ETL-Step-${var.environment}"
+  filename      = "${path.module}/../lambda_functions/add_portal_etl_emr_step_archive.zip"
+  role          = aws_iam_role.lambda_service_role.arn
+  handler       = "main.add_portal_etl_emr_step"
   environment {
     variables = {
       FHIR_SECRETS_NAME = aws_secretsmanager_secret.fhir_secret.name
