80e6fa07b9e359fe1f05871943a432191e288dae
Evans Girard <54366437+evans-g-crsj@users.noreply.github.com>
2024-03-26 17:53:12 -0400

    :wastebasket: Remove debug logs in Fhavro export (#347)

--- fhavro-export/src/main/scala/bio/ferlab/fhir/etl/fhir/FhirUtils.scala
+++ fhavro-export/src/main/scala/bio/ferlab/fhir/etl/fhir/FhirUtils.scala
@@ -7,14 +7,10 @@ import ca.uhn.fhir.rest.client.impl.GenericClient
 import ca.uhn.fhir.rest.client.interceptor.{BearerTokenAuthInterceptor, LoggingInterceptor}
 import org.apache.http.HttpHost
 import org.apache.http.client.utils.URIUtils
-import org.slf4j.{Logger, LoggerFactory}
 
 import java.net.URI
 
 object FhirUtils {
-
-  val LOGGER: Logger = LoggerFactory.getLogger(getClass)
-
   private def isUpgradedServer(url: String) = {
     //https://github.com/kids-first/kf-api-fhir-service/blob/eacdf0771ec87da1428b58cf915b78106cc3d801/README.md?plain=1#L19
     List(
@@ -37,20 +33,13 @@ object FhirUtils {
       loggingInterceptor.setLogRequestBody(true)
       fhirClient.registerInterceptor(loggingInterceptor);
     }
-    LOGGER.info(s"isUpgradedServer? ${isUpgradedServer(fhir_url)}") //TODO: remove
+
     if (isUpgradedServer(fhir_url)) {
-      LOGGER.info(s"About to fetch access token") //TODO: remove
-      LOGGER.info(s"Token Request is: url=${tr.url} client-id=${tr.clientId} grant-type=${tr.grantType} client-secret=${tr.clientSecret}") //TODO: remove
       val token: Either[String, String] = KcTokenHandler.fetch(
         tr
       )
 
-      if (token.isLeft) { //TODO: remove
-        LOGGER.info(s"Error fetching token. Got ${token.left.getOrElse("no value")}")
-      }
-
       token.map { s =>
-        LOGGER.info(s"Using Token ${s}") //TODO: remove
         fhirClient.registerInterceptor(new BearerTokenAuthInterceptor(s))
         fhirClient
       }
